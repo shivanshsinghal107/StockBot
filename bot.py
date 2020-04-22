877c8fa661d2ceada2c0d347e55ee9bec06f1c5a
Shivansh Singhal <shivanshsinghal107@gmail.com>
2020-04-22 16:59:08 +0530

    Added functionality to get commodity prices in real time
    Commodity thing done, now you can get the commodity list the bot has the data for by command /commlist and bot can get you the commodity price by command /comm "commodity name".

--- bot.py
+++ bot.py
@@ -3,16 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-'''
-# set up the logging module, so you will know when (and why) things don't work as expected
-import logging
-logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
 
-updater = Updater(token = TOKEN, use_context = True)
-'''
-
-# Now, you can define a function that should process a specific type of update
-
+# function to get stock prices of indexes of given country
 def index_price(country):
     # load csv and get dataframe
     df = pd.read_csv('stock_indices.csv')
@@ -54,27 +46,68 @@ def index_price(country):
 
     return indexes
 
+# function to get commodity price for the given commodity
+def commodity_price(commodity):
+    # load csv and get dataframe
+    df = pd.read_csv('commodities.csv')
+
+    # check which commodity user has entered and get it
+    indice = df[df['Name'].str.contains(commodity.upper())].reset_index()
+    indice.drop(columns = ['index'], inplace = True)
+
+    # if indice is empty that means the given commodity is not in the database
+    if(indice.empty):
+        return " "
+
+    # webpage having the commodity's stock price
+    url = indice['URL'][0]
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # getting stock price
+    price = soup.find_all('div', {'class' : 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+
+    # getting currency string
+    c = soup.find_all('div', {'class' : 'C($tertiaryColor) Fz(12px)'})[0].find('span').text
+
+    # getting required string for currency by splliting by dot(.)
+    currency = c.rsplit('.')[1]
+    # removing a left spaces from string
+    currency = currency.lstrip()
+    # removing extra words
+    currency = currency.replace("Currency in ", "")
+
+    index = "Currently the " + str(indice['Name'][0]) + " stands at " + str(price) + " " + str(currency) + "\n"
+
+    return index
+
 
 def start(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = "Hey there, Wanna know about Stock Market and stuff?")
 
+# function to display country list the bot has
 def get_country_list(update, context):
         df = pd.read_csv('stock_indices.csv')
-        countries = "Here is your list:\n"
+        countries = "Here is your country list:\n"
         # really smart move
         for c in df['Country']:
             countries += c + "\n"
         context.bot.send_message(chat_id = update.effective_chat.id, text = countries)
 
+# function to display commodities list the bot has
+def get_commodity_list(update, context):
+    df = pd.read_csv('commodities.csv')
+    commodities = "Here are your commodities:\n"
+    for c in df['Name']:
+        commodities += c + "\n"
+    context.bot.send_message(chat_id = update.effective_chat.id, text = commodities)
+
+# function to display stock prices of stock indexes for a given country
 def get_stock_prices(update, context):
-    # taking country input from user
-    # country = input("Tell me the country name: ")
     if context.args == []:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='Type in the country name as the KeyWord along with the index command')
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Type in the country name as the KeyWord along with the /index command')
         return
-    #if len(context.args) != 1:
-    #    context.bot.send_message(chat_id=update.effective_chat.id, text='Only one argument allowed')
-    #    return
     context.bot.send_message(chat_id=update.effective_chat.id, text= "Just a sec! Finding related info!")
     country = " ".join(context.args)
     stock_indices = index_price(country)
@@ -82,6 +115,19 @@ def get_stock_prices(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text= "Sorry, I don't have data for this. Take a look at the country list by command /country.")
     else:
         context.bot.send_message(chat_id=update.effective_chat.id, text = stock_indices)
+
+# function to display commodity price for a given commodity
+def get_commodity_prices(update, context):
+    if context.args == []:
+        context.bot.send_message(chat_id = update.effective_chat.id, text = 'Type in the commodity as the KeyWord along with the /comm command.')
+        return
+    context.bot.send_message(chat_id = update.effective_chat.id, text = "Just a sec! Finding related info!")
+    commodity = " ".join(context.args)
+    commodities = commodity_price(commodity)
+    if(commodities == " "):
+        context.bot.send_message(chat_id=update.effective_chat.id, text= "Sorry, I don't have data for this. Take a look at the country list by command /commlist.")
+    else:
+        context.bot.send_message(chat_id = update.effective_chat.id, text = commodities)
 
 
 
@@ -97,6 +143,10 @@ def main():
     dp.add_handler(stock_handler)
     country_handler = CommandHandler('country', get_country_list)
     dp.add_handler(country_handler)
+    commodity_handler = CommandHandler('commlist', get_commodity_list)
+    dp.add_handler(commodity_handler)
+    commodity_price_handler = CommandHandler('comm', get_commodity_prices)
+    dp.add_handler(commodity_price_handler)
     updater.start_polling()
     updater.idle()
 '''
